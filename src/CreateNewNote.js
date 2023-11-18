373ff2b0a459aacad7386ae692c0b55cf91436ff
Boyan <104321637+BoyanDashev@users.noreply.github.com>
2023-11-18 09:45:25 +0200

    New Updates

--- src/CreateNewNote.js
+++ src/CreateNewNote.js
@@ -1,9 +1,23 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function CreateNewNode() {
+  const textAreaRef = useRef(null);
+  const anotherTextAreaRef = useRef(null);
   const [valuefromtext, setValueFromText] = useState("");
-  const [paragraphs, setParagraphs] = useState([]);
   const [valuefromanothertext, setValueFromAnotherText] = useState("");
+
+  useEffect(() => {
+    textAreaRef.current.style.height = "auto";
+    textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
+  }, [valuefromtext]);
+
+  useEffect(() => {
+    anotherTextAreaRef.current.style.height = "auto";
+    anotherTextAreaRef.current.style.height = `${anotherTextAreaRef.current.scrollHeight}px`;
+  }, [valuefromanothertext]);
+
+  const [paragraphs, setParagraphs] = useState([]);
+
   const [textitem, setTextItem] = useState([]);
 
   const addTextToParagraphs = () => {
@@ -14,41 +28,76 @@ function CreateNewNode() {
   };
 
   const removeParagraph = (index) => {
-    const updatedParagraphs = paragraphs.filter(( idx) => idx !== index);
-    const updatedTextItem = textitem.filter(( idx) => idx !== index);
+    const updatedParagraphs = paragraphs.filter((_, idx) => idx !== index);
+    const updatedTextItem = textitem.filter((_, idx) => idx !== index);
     setParagraphs(updatedParagraphs);
     setTextItem(updatedTextItem);
   };
 
   return (
-    <div>
-      <div className="fixed top-20 left-1/2 transform -translate-x-1/3 w-full">
+    <div className="overflow-y-auto  h-screen">
+      <div className="absolute top-20 left-1/2 transform -translate-x-1/3 w-full">
         <div className="grid gap-2 mx-auto w-1/2">
           <textarea
             value={valuefromtext}
-            onChange={(e) => setValueFromText(e.target.value)}
+            onChange={(e) => {
+              setValueFromText(e.target.value);
+              textAreaRef.current.style.height = "auto";
+              textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
+            }}
+            rows="2"
+            ref={textAreaRef}
             placeholder="Title"
-            className="h-10 w-1/2 border-solid border-2 border-gray-600"
+            className="text-xl rounded-md h-10 w-1/2 border-solid border-2 border-gray-600"
           ></textarea>
           <textarea
             value={valuefromanothertext}
-            onChange={(e) => setValueFromAnotherText(e.target.value)}
+            onChange={(e) => {
+              setValueFromAnotherText(e.target.value);
+              anotherTextAreaRef.current.style.height = "auto";
+              anotherTextAreaRef.current.style.height = `${anotherTextAreaRef.current.scrollHeight}px`;
+            }}
+            rows="2"
+            ref={anotherTextAreaRef}
             placeholder="Take a note..."
-            className="h-20 w-1/2 border-solid border-2 border-gray-600"
+            className="rounded-md h-20 w-1/2 border-solid border-2 border-gray-600"
           ></textarea>
         </div>
         <div>
-          <ul className="grid gap-2 mx-auto w-2/3">
+          <div className="mt-10 mx-auto max-w-1/3 flex flex-col gap-4">
             {paragraphs.map((text, index) => (
-              <div key={index}>
-                <p>{text}</p>
-                <button onClick={() => removeParagraph(index)}>Delete</button>
+              <div
+                key={index}
+                className="rounded border-solid border-2 border-gray-600 w-1/6"
+                style={{ minWidth: "16.666%" }}
+              >
+                <div className="p-4">
+                  <div className="text-xl max-w-15pc break-words">{text}</div>
+                </div>
               </div>
             ))}
             {textitem.map((text, index) => (
-              <p key={index}>{text}</p>
+              <div
+                key={index}
+                className="rounded border-solid border-2 border-gray-600 w-1/6"
+                style={{ minWidth: "16.666%" }}
+              >
+                <div className="text-xl max-w-15pc break-words">{text}</div>
+                <div
+                  className="w-5 h-5 mt-3"
+                  onClick={() => removeParagraph(index)}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 18 20"
+                  >
+                    <path d="M17 4h-4V2a2 2 0 0 0-2-2H7a2 2 0 0 0-2 2v2H1a1 1 0 0 0 0 2h1v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6h1a1 1 0 1 0 0-2ZM7 2h4v2H7V2Zm1 14a1 1 0 1 1-2 0V8a1 1 0 0 1 2 0v8Zm4 0a1 1 0 0 1-2 0V8a1 1 0 0 1 2 0v8Z" />
+                  </svg>
+                </div>
+              </div>
             ))}
-          </ul>
+          </div>
         </div>
       </div>
       <div>
